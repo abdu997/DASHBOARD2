1acf6355af942b2761e16eea9729fb5954b3a14d
ybce <elkhalili.youssef@outlook.com>
2017-07-18 13:11:10 -0400

    Commit

--- php/chatroom.php
+++ php/chatroom.php
@@ -1,24 +1,32 @@
 <?php
 include "connection.php";
 
+session_start();
+
+
 $user_id = $_SESSION['user_id'];
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-
-//$sql = "INSERT INTO chatrooms (chatroom_name) 
-// VALUES ('".$"chatroom_name"]."')";
+$tid = $_SESSION['team_id'];
 
 
-//  $chatroom_name = $_POST['chatroom_name'];
-//  $query = "INSERT INTO `chatrooms` (`chatroom_name`) VALUES ('$chatroom_name');";
-//
-//  mysql_query($query);
 
-    $statement = $mysqli->prepare("INSERT INTO chatrooms (chatroom_id, chatroom_name, team_id) VALUES(,$chatroom_name)"); //prepare sql insert query
-    //bind parameters for markers, where (s = string, i = integer, d = double,  b = blob)
-    $statement->bind_param('sss', $u_name, $u_email, $u_text); //bind values and execute insert query
+
+if(isset($_POST['chatroom_name'])){
+    $chatroom_name = $_POST['chatroom_name'];
+    $chatroom_name = mysqli_real_escape_string($conn,$_POST['chatroom_name']);
+}
+else{
+       echo "Something went wrong with post";
+   }
+   
+   $abdul = "INSERT INTO chatrooms (chatroom_name,team_id) VALUES ('$chatroom_name', '$tid')";
+   if(mysqli_query($conn, $abdul)){
+       header("Location: ../chat.php");
+   }
+   else{
+       echo "error";
+   }
+   
+   
+
 
 ?>
